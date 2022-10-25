35fcbbc72d558320d9d629621339b441cbecb9a5
aluno <aluno@ifpe>
2022-10-25 20:15:16 -0300

    Primeiro commit

---
+++ src/main/java/br/com/ifpe/pkafkaproducer/PkafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.pkafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkafkaProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/pkafkaproducer/PkafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.pkafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
